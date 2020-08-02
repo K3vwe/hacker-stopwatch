ff244a82b9d19335344aa20b266e05faecfbc3c7
k3vwe <rukkimax@gmail.com>
2020-08-02 07:23:36 +0100

    fix the problem with button combination click conflict

--- stopwatch.js
+++ stopwatch.js
@@ -4,7 +4,9 @@ let startBtn = document.querySelector('#start');
 let pauseBtn = document.querySelector('#pause');
 let resetBtn = document.querySelector('#reset');
 let screen = document.querySelector('#screen');
-let timer, currentTime, pauseSec, continueTime;
+let timer, currentTime, pauseSec, continueTime, timer2;
+var startState = false, resetState = false, pauseState = false, resumeState = false;
+
 
 // Function to make sure the value is 2-Digits
 function pad(val){
@@ -24,6 +26,7 @@ function setTime(value){
 
 // Start listener to start the stop watch
 startBtn.addEventListener('click', function(){
+    startState = true;
     startBtn.setAttribute('disabled', 'disabled');
 
     // Get current time in ms;
@@ -41,28 +44,37 @@ startBtn.addEventListener('click', function(){
 // Add a pause btn feature to stop the current counter
 let pause = true;       // Pause status flag
 pauseBtn.addEventListener('click', function(){
-    if(!pause){  // When pause is clicked at second state
-        // Continue stopwatch timer from where it left off
-        var resumeTime = Date.now();
-        // Get in ms not in sec;
-        timer = setInterval( ()=> {
-            continueTime = Date.now() - resumeTime;
-            setTime( (continueTime/1000) + pauseSec);
-        } ,1000)
-        pauseBtn.textContent = "Pause";
-
-    } else {    // When pause is clicked at first state
-        pauseBtn.textContent = "Resume";
-        pauseSec = currentTime/1000;    // Time in Sec timer was paused
-        clearInterval(timer);
-        // console.log(pauseSec);
+    if(!startState){
+        setTime(0);
+    } else {
+        if(!pause){  // When pause is clicked at second state
+            // Continue stopwatch timer from where it left off
+            var resumeTime = Date.now();
+            // Get in ms not in sec;
+            timer2 = setInterval( ()=> {
+                continueTime = Date.now() - resumeTime;
+                setTime( (continueTime/1000) + pauseSec);
+            } ,1000)
+            pauseBtn.textContent = "Pause";
+    
+        } else {    // When pause is clicked at first state
+            pauseBtn.textContent = "Resume";
+            pauseSec = currentTime/1000;    // Time in Sec timer was paused
+            clearInterval(timer);
+            clearInterval(timer2);
+            // console.log(pauseSec);
+        }
+        pause = !pause;
     }
-    pause = !pause;
+    
 });
 
 // Reset stopwatch timer
 resetBtn.addEventListener('click', function(){
+    startState = false;
     clearInterval(timer);
+    clearInterval(timer2);
+    pauseBtn.textContent = "Pause";
     startBtn.removeAttribute('disabled');
     setTime(0);
 });
